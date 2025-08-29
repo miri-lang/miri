3c699f8036282956065a8720a18737226eeb6d9a
Slavik Shynkarenko <slavik@slavikdev.com>
2025-08-28 23:26:07 -0400

    Renamed test files

--- tests/lexer/mod.rs
+++ tests/lexer/mod.rs
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2017–2025 Viacheslav Shynkarenko
 
-pub mod basics;
-pub mod comments;
+pub mod basic;
+pub mod comment;
 pub mod conditional_expressions;
-pub mod functions;
-pub mod identifiers;
+pub mod function;
+pub mod identifier;
 pub mod if_statement;
 pub mod indentation;
-pub mod keywords;
-pub mod loops;
-pub mod numbers;
-pub mod operators;
+pub mod keyword;
+pub mod r#loop;
+pub mod number;
+pub mod operator;
 pub mod regex;
-pub mod strings;
-pub mod symbols;
+pub mod string;
+pub mod symbol;
 pub mod use_statement;
 pub mod utils;
-pub mod variables;
+pub mod variable;
