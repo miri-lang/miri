fd70ff04b823b6ab300e6f552349b1d42973082c
Slavik Shynkarenko <slavik@slavikdev.com>
2025-12-19 23:03:58 +0100

    Added more type checker tests for formatted strings

--- tests/type_checker/formatted_string.rs
+++ tests/type_checker/formatted_string.rs
@@ -44,3 +44,63 @@ fn test_formatted_string_type_error_in_expression() {
         "Invalid types for arithmetic operation",
     );
 }
+
+#[test]
+fn test_formatted_string_function_call() {
+    check_success(
+        "
+fn get_name() string: \"World\"
+let s = f\"Hello {get_name()}\"
+",
+    );
+}
+
+#[test]
+fn test_formatted_string_complex_expression() {
+    check_expr_type("f\"Value: {1 + 2 * 3}\"", Type::String);
+    check_expr_type("f\"Bool: {true and false}\"", Type::String);
+}
+
+#[test]
+fn test_formatted_string_member_access() {
+    check_success(
+        "
+struct Point: x int, y int
+let p = Point(1, 2)
+let s = f\"Point: {p.x}, {p.y}\"
+",
+    );
+}
+
+#[test]
+fn test_formatted_string_empty() {
+    check_expr_type("f\"\"", Type::String);
+}
+
+#[test]
+fn test_formatted_string_only_expression() {
+    check_expr_type("f\"{1}\"", Type::String);
+}
+
+#[test]
+fn test_formatted_string_void_expression() {
+    // Void expressions are currently allowed in formatted strings
+    check_success(
+        "
+fn void_func()
+    return
+let s = f\"Void: {void_func()}\"
+",
+    );
+}
+
+#[test]
+fn test_formatted_string_multiline() {
+    check_expr_type(
+        "f\"
+    Line 1
+    Line 2 {1}
+    \"",
+        Type::String,
+    );
+}
